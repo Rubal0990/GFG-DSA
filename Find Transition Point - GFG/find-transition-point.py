ac2382d51b08f60f583457b223ce777d3d81f44b
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2022-01-10 19:33:51 +0530

    Added solution - LeetHub

---
+++ Find Transition Point - GFG/find-transition-point.py
@@ -0,0 +1,21 @@
+def transitionPoint(arr, n):
+    if n == 1 and arr[0] == 1:
+        return 0
+    
+    for i in range(n):
+        if arr[i] == 1:
+            return i
+    
+    return -1
+    
+#{ 
+#  Driver Code Starts
+#driver code
+if __name__=='__main__':
+    t=int(input())
+    for i in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        print(transitionPoint(arr, n))
+
+# } Driver Code Ends
