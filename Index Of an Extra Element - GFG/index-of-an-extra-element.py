43ed081aa08c9450d2e724a673c3116a9ee39464
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2022-01-14 13:28:15 +0530

    Added solution - LeetHub

---
+++ Index Of an Extra Element - GFG/index-of-an-extra-element.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findExtra(self,a,b,n):
+        for i in range(len(b)):
+            if a[i] != b[i]:
+                return i
+            
+        return n-1
+
+#{ 
+#  Driver Code Starts
+if __name__ == "__main__":
+    t=int(input())
+    for i in range(t):
+        n=int(input())
+        a=list(map(int,input().split()))
+        b=list(map(int,input().split()))
+        print(Solution().findExtra(a,b,n))
+# } Driver Code Ends
