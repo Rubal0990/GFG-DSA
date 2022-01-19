9eac764a864b89e64286afed1af0def6c97f2a02
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2022-01-19 11:49:46 +0530

    Added solution - LeetHub

---
+++ Maximum sum of increasing order elements from n arrays - GFG/maximum-sum-of-increasing-order-elements-from-n-arrays.py
@@ -0,0 +1,40 @@
+#User function Template for python3
+
+def maximumSum (n, m, arr) : 
+    a=[]
+    temp=max(arr[n-1])
+    sum=temp
+    
+    for i in range(n-1,0,-1):
+        for item in arr[i-1]:
+            if item < temp:
+                a.append(item)
+                
+        if(len(a)==0):
+            return 0
+            
+        temp=max(a)
+        sum+=temp
+        a=[]
+        
+    return sum
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+
+for _ in range(0,int(input())):
+    n, m = map(int , input().split())
+    arr = []
+    for i in range(0, n):
+        ll = list(map(int, input().strip().split()))
+        arr.append(ll)
+    ans = maximumSum(n, m, arr)
+    print(ans)
+
+
+
+
+# } Driver Code Ends
