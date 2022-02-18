e145b127329ce39a946faf9a2d65ae32c2491a2f
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2022-02-18 10:16:06 +0530

    Added solution - LeetHub

---
+++ Rearrange Geek and his Classmates - GFG/rearrange-geek-and-his-classmates.py
@@ -0,0 +1,24 @@
+#User function Template for python3
+
+class Solution:
+    def prank(self, a, n): 
+        b = a.copy()
+        
+        for i in range(n):
+        	a[i] = b[b[i]]
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        a = list(map(int, input().strip().split()))
+        ob = Solution()
+        ob.prank(a, n)
+        for i in a:
+            print(i,end=" ")
+        print()
+# } Driver Code Ends
