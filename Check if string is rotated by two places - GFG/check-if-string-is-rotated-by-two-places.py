db025ae49827b716832e9783d77e71628f83cb3e
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2022-02-28 11:12:02 +0530

    Added solution - LeetHub

---
+++ Check if string is rotated by two places - GFG/check-if-string-is-rotated-by-two-places.py
@@ -0,0 +1,44 @@
+# User function Template for python3
+
+class Solution:
+    def isRotated(self,str1,str2):
+        if len(str1) != len(str2):
+            return 0
+            
+        if str1 == str2:
+            return 1
+        
+        if len(str1) > 2:
+            if str1[2:]+str1[:2] == str2 or str1[-2:]+str1[:-2] == str2:
+                return 1
+        else:
+            return 0
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        s=str(input())
+        p=str(input())
+        if(Solution().isRotated(s,p)):
+            print(1)
+        else:
+            print(0)
+# } Driver Code Ends
