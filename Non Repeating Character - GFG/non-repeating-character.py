578c0ca6b1102090d25594ae7171ce95d0d4e989
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2022-02-28 11:06:31 +0530

    Added solution - LeetHub

---
+++ Non Repeating Character - GFG/non-repeating-character.py
@@ -0,0 +1,48 @@
+#User function Template for python3
+
+class Solution:
+    def nonrepeatingCharacter(self,s):
+        d = {}
+        for i in range(len(s)):
+            if s[i] not in d:
+                d[s[i]] = 0
+           
+            d[s[i]] +=1
+           
+        for i in s:
+            if d[i] == 1:
+                return i
+                
+        return '$'
+    
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        s=str(input())
+        obj = Solution()
+        ans=obj.nonrepeatingCharacter(s)
+        if(ans!='$'):
+            print(ans)
+        else:
+            print(-1)
+            
+# } Driver Code Ends
