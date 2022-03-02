990b6bf68bb8d7cdf95595bb2cb122e606c0df17
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2022-03-02 11:07:40 +0530

    Added solution - LeetHub

---
+++ Exactly one swap - GFG/exactly-one-swap.py
@@ -0,0 +1,34 @@
+#User function Template for python3
+class Solution:
+    def countStrings(self, S): 
+        n = len(S)
+        charSet = {}
+        count = 0
+        
+        for i in S:
+            if i in charSet:
+                count += charSet[i] + 1
+                charSet[i] += 1
+            else:
+                charSet[i] = 0
+        
+        ans = (n*(n-1)) // 2
+        if(count >0):
+            ans = ans - count + 1
+        
+        return ans
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        S = input()
+        ob = Solution()
+        ans = ob.countStrings(S)
+        print(ans)
+# } Driver Code Ends
