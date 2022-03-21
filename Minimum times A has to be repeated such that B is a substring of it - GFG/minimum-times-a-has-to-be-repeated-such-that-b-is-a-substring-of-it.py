f513699bedf412e73eee61f323fb2441b5271283
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2022-03-21 09:23:37 +0530

    Added solution - LeetHub

---
+++ Minimum times A has to be repeated such that B is a substring of it - GFG/minimum-times-a-has-to-be-repeated-such-that-b-is-a-substring-of-it.py
@@ -0,0 +1,25 @@
+#User function Template for python3
+
+class Solution:
+    def minRepeats(self, A, B):
+        a = len(B) // len(A)
+        for x in range(len(A) + 1):
+            s = (a + x) * A
+            if B in s:
+                return a + x
+        
+        return -1
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        A=input()
+        B=input()
+        
+        ob = Solution()
+        print(ob.minRepeats(A,B))
+# } Driver Code Ends
