59b962747196c879736477254f58ed06e8e4f825
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2022-04-26 11:12:46 +0530

    Added solution - LeetHub

---
+++ Eulerian Path in an Undirected Graph - GFG/eulerian-path-in-an-undirected-graph.py
@@ -0,0 +1,30 @@
+# User function Template for Python3
+
+class Solution:
+    def eulerPath(self, N, graph):
+        evencount = 0
+        
+        for i in range(N):
+            if sum(graph[i])%2 == 0:
+                evencount += 1
+                
+        if evencount == (N-2):
+            return 1
+            
+        return 0
+
+#{ 
+#  Driver Code Starts
+# Initial Template for Python3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input().strip())
+        graph = []
+        for i in range(N):
+            graph.append(list(map(int, input().strip().split())))
+        
+        ob = Solution()
+        print(ob.eulerPath(N, graph))
+# } Driver Code Ends
