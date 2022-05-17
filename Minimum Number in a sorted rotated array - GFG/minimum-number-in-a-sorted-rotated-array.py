d53252ab1471823003d8ceb666b7ee0b8373b250
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2022-05-17 23:00:16 +0530

    Added solution - LeetHub

---
+++ Minimum Number in a sorted rotated array - GFG/minimum-number-in-a-sorted-rotated-array.py
@@ -0,0 +1,30 @@
+#User function Template for python3
+
+
+class Solution:
+    def minNumber(self, arr, low, high):
+        return min(arr)
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+def main():
+        T=int(input())
+        while(T>0):
+            
+            N=int(input())
+
+            A=[int(x) for x in input().strip().split()]
+            obj = Solution()
+            print(obj.minNumber(A,0,N-1))
+            
+            T-=1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
