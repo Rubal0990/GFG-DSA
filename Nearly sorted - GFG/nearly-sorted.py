d581957cf960395e449ff0b1185e31749739d735
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2022-06-08 04:35:26 +0530

    Added solution - LeetHub

---
+++ Nearly sorted - GFG/nearly-sorted.py
@@ -0,0 +1,26 @@
+#User function Template for python3
+
+class Solution:
+    def nearlySorted(self,a,n,k):
+        return sorted(a)
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+import heapq
+from collections import  defaultdict
+
+# Contributed by : Nagendra Jha
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n,k = map(int,input().strip().split())
+        a = list(map(int,input().strip().split()))
+        ob=Solution()
+        print(*ob.nearlySorted(a,n,k))
+
+# } Driver Code Ends
