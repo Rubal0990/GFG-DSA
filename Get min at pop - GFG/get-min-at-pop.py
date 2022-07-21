af28a23fa5950bda08e44a925278e9b5ed244ce7
Rubal0990 <rubal00009@gmail.com>
2022-07-21 09:38:56 +0530

    Added solution - LeetHub

---
+++ Get min at pop - GFG/get-min-at-pop.py
@@ -0,0 +1,45 @@
+#User function Template for python3
+
+#Function to push all the elements into the stack.
+def _push(a,n):
+    return a
+
+
+#Function to print minimum value in stack each time while popping.    
+def _getMinAtPop(stack):
+    for _ in range(len(stack)):
+        minN = min(stack)
+        stack.pop()
+        print(minN, end=" ")
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+#Contributed by : Nagendra Jha
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n = int(input())
+        a = list(map(int,input().strip().split()))
+        stack =  _push(a,n) # our stack to be used
+        _getMinAtPop(stack) # print elements of stack as required
+        print()
+
+# } Driver Code Ends
