75569aade6e11acb372165d141106ded80ee7636
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2022-07-28 20:57:27 +0530

    Python - Strings

--- Edit Distance - GFG/edit-distance.py
+++ Edit Distance - GFG/edit-distance.py
@@ -1,27 +1,21 @@
 class Solution:
-    def soln(self,s,t,i,j,dp):
-        if i==0 or j==0:
-            return max(i, j)
-        
-        if dp[i][j] != -1:
-            return dp[i][j]
-        else:
-            if s[i-1] == t[j-1]:
-                dp[i][j] = self.soln(s, t, i-1, j-1, dp)
-                return dp[i][j]
-            else:
-                dp[i][j] = 1 + min(self.soln(s, t, i-1, j-1, dp), self.soln(s, t, i, j-1, dp), self.soln(s, t, i-1, j, dp))
-            
-            return dp[i][j]
-    
-	def editDistance(self, s, t):
-        # Code here
-        n = len(s)
-        m = len(t)
-        dp = [[-1 for _ in range(m+1)] for _ in range(n+1)]
-        
-        return self.soln(s, t, n, m, dp)
+	def editDistance(self, word1, word2):
+        dp = [[float("inf")] * (len(word2) + 1) for i in range(len(word1) + 1)]
 
+        for j in range(len(word2) + 1):
+            dp[len(word1)][j] = len(word2) - j
+        
+        for i in range(len(word1) + 1):
+            dp[i][len(word2)] = len(word1) - i
+
+        for i in range(len(word1) - 1, -1, -1):
+            for j in range(len(word2) - 1, -1, -1):
+                if word1[i] == word2[j]:
+                    dp[i][j] = dp[i + 1][j + 1]
+                else:
+                    dp[i][j] = 1 + min(dp[i + 1][j], dp[i][j + 1], dp[i + 1][j + 1])
+        
+        return dp[0][0]
 
 #{ 
  # Driver Code Starts
