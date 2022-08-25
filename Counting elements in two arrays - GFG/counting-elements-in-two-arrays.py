0f4f190f0917527a9d4ee71831b164527c1ddc69
Rubal0990 <rubal00009@gmail.com>
2022-08-25 08:15:28 +0530

    Python - Binary Search

---
+++ Counting elements in two arrays - GFG/counting-elements-in-two-arrays.py
@@ -0,0 +1,33 @@
+#User function Template for python3
+import bisect
+
+class Solution:
+    def countEleLessThanOrEqual(self, arr1, n1, arr2, n2):
+        arr2.sort()
+        res = []
+        
+        for num in arr1:
+            res.append(bisect.bisect_right(arr2,num))
+        return res
+    
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+if __name__ == '__main__':
+    t=int(input())
+    
+    for tcs in range(t):
+        
+        n1,n2=[int(x) for x in input().split()]
+        arr1=[int(x) for x in input().split()]
+        arr2=[int(x) for x in input().split()]
+    
+        res = Solution().countEleLessThanOrEqual(arr1,n1,arr2,n2)
+        for i in range (len (res)):
+            print (res[i], end = " ")
+        print()
+# } Driver Code Ends
