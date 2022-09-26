062621f86220a34e4c9e06bc89cdc12f47eb556a
Rubal Bansal <rubal00009@gmail.com>
2022-09-27 00:00:08 +0530

    Python - Searching

---
+++ Smallest Positive missing number - GFG/smallest-positive-missing-number.py
@@ -0,0 +1,40 @@
+#User function Template for python3
+
+class Solution:
+    def missingNumber(self, arr, n):
+        arr.sort()
+        ans = 1
+        
+        for i in range(n):
+            if arr[i] == ans:
+                ans += 1
+        
+        return ans
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+import math
+
+
+def main():
+        T=int(input())
+        while(T>0):
+            
+            n=int(input())
+            
+            arr=[int(x) for x in input().strip().split()]
+            
+            ob=Solution()
+            print(ob.missingNumber(arr,n))
+            
+            T-=1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
