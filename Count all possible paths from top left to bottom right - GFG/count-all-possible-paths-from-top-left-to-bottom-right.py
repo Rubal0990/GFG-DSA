3e16695fd898a814098be2c4fb1023da74630f79
Rubal Bansal <rubal00009@gmail.com>
2022-10-04 06:43:27 +0530

    Python - DP

---
+++ Count all possible paths from top left to bottom right - GFG/count-all-possible-paths-from-top-left-to-bottom-right.py
@@ -0,0 +1,22 @@
+#User function Template for python3
+from math import factorial
+
+class Solution:
+	def numberOfPaths(self, m, n):
+		return (factorial(m+n-2) // (factorial(n-1) * factorial(m-1))) % (1000000007)
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		m,n = input().split()
+		m=int(m)
+		n=int(n)
+		ob = Solution();
+		print(ob.numberOfPaths(m,n))
+
+# } Driver Code Ends
