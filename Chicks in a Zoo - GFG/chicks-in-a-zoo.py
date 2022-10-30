2314a2f4e52cf3aa20377487f32f8a88e38730de
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2022-10-30 23:58:25 +0530

    Update chicks-in-a-zoo.py

--- Chicks in a Zoo - GFG/chicks-in-a-zoo.py
+++ Chicks in a Zoo - GFG/chicks-in-a-zoo.py
@@ -3,19 +3,19 @@
 class Solution:
 	def NoOfChicks(self, N):
 		p = []
-        for i in range(0, N+1):
-            p.append(1)
-        
-        q, p[0] = 1, 1
-        
-        for i in range(1, N):
-            if i >= 6:
-                q -= p[i-6]
-            
-            p[i] =  2 * q
-            q += p[i]
-        
-        return q
+		for i in range(0, N+1):
+		    p.append(1)
+
+		q, p[0] = 1, 1
+
+		for i in range(1, N):
+		    if i >= 6:
+			q -= p[i-6]
+
+		    p[i] =  2 * q
+		    q += p[i]
+
+		return q
 
 
 #{ 
