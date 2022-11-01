a6eb4fc4e23095d771758ba0954e9c82bc2aca77
Rubal Bansal <rubal00009@gmail.com>
2022-11-01 10:40:21 +0530

    Python - Binary Search | Matrix

--- Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.py
+++ Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.py
@@ -1,17 +1,28 @@
 #User function Template for python3
+from bisect import bisect
 
 class Solution:
-    def median(self, matrix, r, c):
-        lst = []
-    	for i in range(r):
-    	    for j in range(c):
-    	        lst.append(matrix[i][j])
-    	
-    	lst.sort()
-    	return lst[len(lst)//2]
+    def median(self, matrix, R, C):
+    	low = 1
+        high = 2001
+        
+        while low < high:
+            mid = (low+high) // 2
+            count = 0
+            
+            for row in range(R):
+                count += bisect(matrix[row], mid)
+            
+            if count <= (R*C)//2:
+                low = mid + 1
+            else:
+                high = mid
+        
+        return low
+
 
 #{ 
-#  Driver Code Starts
+ # Driver Code Starts
 #Initial Template for Python 3
 
 if __name__ == '__main__': 
@@ -20,12 +31,10 @@ if __name__ == '__main__':
     for _ in range (t):
         r,c = map(int,input().strip().split())
         matrix = [[0 for j in range(c)] for i in range(r)]
-        line1 = [int(x) for x in input().strip().split()]       
-        k = 0
         for i in range(r):
-            for j in range (c):
-                matrix[i][j]=line1[k]
-                k+=1
+            t=[int(el) for el in input().split()]
+            for j in range(c):
+                matrix[i][j]=t[j]
         ans = ob.median(matrix, r, c);
         print(ans)
 # } Driver Code Ends
