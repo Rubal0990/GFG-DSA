5a76460c0b3ff8ef2f1a71a5ef98a01091c2d2ba
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2022-11-27 23:13:42 +0530

    Update add-binary-strings.py

--- Add Binary Strings - GFG/add-binary-strings.py
+++ Add Binary Strings - GFG/add-binary-strings.py
@@ -1,32 +1,32 @@
 #User function Template for python3
 class Solution:
 	def addBinary(self, A, B):
-		while A and A[0] == '0':
-            A = A[1:]
-        while B and B[0] == '0':
-            B = B[1:]
+	    while A and A[0] == '0':
+                A = A[1:]
+            while B and B[0] == '0':
+                B = B[1:]
         
-        res = ""
-        carry, i, j = 0, len(A)-1, len(B)-1
-        
-        while i >= 0 or j >= 0 or carry > 0:
-            if i >= 0:
-                x = int(A[i])
-            else:
-                x = 0
+            res = ""
+            carry, i, j = 0, len(A)-1, len(B)-1
             
-            if j >= 0:
-                y = int(B[j])
-            else:
-                y = 0
+            while i >= 0 or j >= 0 or carry > 0:
+                if i >= 0:
+                    x = int(A[i])
+                else:
+                    x = 0
+            
+                if j >= 0:
+                    y = int(B[j])
+                else:
+                    y = 0
                 
-            v = x + y + carry
-            carry = v // 2
-            v %= 2
-            res += str(v)
-            i, j = i-1, j-1
+                v = x + y + carry
+                carry = v // 2
+                v %= 2
+                res += str(v)
+                i, j = i-1, j-1
         
-        return res[: :-1]
+            return res[: :-1]
 
 
 #{ 
