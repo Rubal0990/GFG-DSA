b583a66d1830c5da1d0efe5a22b0bba2f7798d04
Rubal Bansal <rubal00009@gmail.com>
2022-11-30 13:01:33 +0530

    Python - Linked List

---
+++ Reorder List - GFG/reorder-list.py
@@ -0,0 +1,96 @@
+#User function Template for python3
+
+#User function Template for python3
+
+# Node Class    
+class node:
+    def __init__(self, val):
+        self.data = val
+        self.next = None
+
+# Linked List Class
+class Linked_List:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+
+#Back-end complete function Template for Python 3
+class Solution:
+    def reorderList(self,head):
+        slow = head
+        fast = head.next
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        
+        second = slow.next
+        slow.next = None
+        prev = None
+        while second:
+            temp = second.next
+            second.next = prev
+            prev, second = second, temp
+        
+        first, second = head, prev
+        while second:
+            tmp1 = first.next
+            tmp2 = second.next
+            first.next = second
+            second.next = tmp1
+            first = tmp1
+            second = tmp2
+        
+        return head
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# Node Class    
+class node:
+    def __init__(self, val):
+        self.data = val
+        self.next = None
+
+# Linked List Class
+class Linked_List:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert(self, val):
+        if self.head == None:
+            self.head = node(val)
+            self.tail = self.head
+        else:
+            new_node = node(val)
+            self.tail.next = new_node
+            self.tail = new_node
+
+    def createList(self, arr, n):
+        for i in range(n):
+            self.insert(arr[i])
+        return self.head
+
+    def printList(self,head):
+        tmp = head
+        while tmp is not None:
+            print(tmp.data, end=" ")
+            tmp=tmp.next
+        print()
+
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        lis = Linked_List()
+        head = lis.createList(arr, n)
+        ob=Solution()
+        ob.reorderList(head)
+
+        lis.printList(head)
+
+# } Driver Code Ends
