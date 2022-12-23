299e28046a83dc597a638ead6997bfcdb308c81c
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2022-12-23 20:08:35 +0530

    Update burst-balloons.py

--- Burst Balloons - GFG/burst-balloons.py
+++ Burst Balloons - GFG/burst-balloons.py
@@ -1,47 +1,54 @@
-#User function Template for python3
-from math import sqrt
+from typing import List
+import sys
 
 class Solution:
-	def threeDivisors(self, query, q):
-	    m = int(sqrt(max(query)))
-	    prime = [True] * (m + 1)
-	    prime[0], prime[1] = False, False
-	    
-	    for i in range(2, len(prime)):
-	        if not prime[i]:
-	            continue
-	        
-	        for j in range(i*i, len(prime), i):
-	            prime[j] = False
-	    
-	    prime = [i for i, e in enumerate(prime) if e]
-	    
-	    ans = [0] * q
-	    for i in range(q):
-	        for k in prime:
-	            if k*k > query[i]:
-	                break
-	            
-	            ans[i] += 1
-	    
-	    return ans
+    def maxCoins(self, n : int, nums : List[int]) -> int:
+        nums.insert(0, 1)
+        nums.append(1)
+        dp = [[0 for i in range(n + 2)] for j in range(n + 2)]
+        
+        for i in range(n, 0, -1):
+            for j in range(1, n+1):
+                if i > j:
+                    continue
+                
+                maxi = -sys.maxsize-1
+                for ind in range(i, j+1):
+                    cost = nums[i-1] * nums[ind] * nums[j+1] + dp[i][ind-1] + dp[ind+1][j]
+                    maxi = max(maxi, cost)
+                    dp[i][j] = maxi
+        
+        return dp[1][n]
 
 
 #{ 
  # Driver Code Starts
-#Initial Template for Python 3
 
-if __name__ == '__main__':
-	T=int(input())
-	for i in range(T):
-		q = int(input())
-		query = []
-		for _ in range(q):
-			query.append(int(input()))
-		
-		ob = Solution()
-		ans = ob.threeDivisors(query,q)
-		for a in ans:
-			print(a)
+class IntArray:
+    def __init__(self) -> None:
+        pass
+    def Input(self,n):
+        arr=[int(i) for i in input().strip().split()]#array input
+        return arr
+    def Print(self,arr):
+        for i in arr:
+            print(i,end=" ")
+        print()
+
+
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        N = int(input())
+        
+        
+        arr=IntArray().Input(N)
+        
+        obj = Solution()
+        res = obj.maxCoins(N, arr)
+        
+        print(res)
+        
 
 # } Driver Code Ends
