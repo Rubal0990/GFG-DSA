8f14f554782551415c1b5274ef87752ebd402388
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2022-12-29 18:34:57 +0530

    Update asteroid-collision.py

--- Asteroid Collision - GFG/asteroid-collision.py
+++ Asteroid Collision - GFG/asteroid-collision.py
@@ -1,47 +1,43 @@
 #User function Template for python3
-from math import sqrt
 
 class Solution:
-	def threeDivisors(self, query, q):
-	    m = int(sqrt(max(query)))
-	    prime = [True] * (m + 1)
-	    prime[0], prime[1] = False, False
-	    
-	    for i in range(2, len(prime)):
-	        if not prime[i]:
-	            continue
-	        
-	        for j in range(i*i, len(prime), i):
-	            prime[j] = False
-	    
-	    prime = [i for i, e in enumerate(prime) if e]
-	    
-	    ans = [0] * q
-	    for i in range(q):
-	        for k in prime:
-	            if k*k > query[i]:
-	                break
-	            
-	            ans[i] += 1
-	    
-	    return ans
+    def asteroidCollision(self, N, arr):
+        st = []
+        
+        for ele in arr:
+            flag = 1
+            
+            while st and (st[-1] > 0 and ele < 0):
+                if abs(ele) > abs(st[-1]):
+                    st.pop()
+                
+                elif abs(ele) == abs(st[-1]):
+                    flag = 0
+                    st.pop()
+                    break
+                
+                else:
+                    flag = 0
+                    break
+            
+            if flag == 1:
+                st.append(ele)
+        
+        return st
 
 
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
 
-if __name__ == '__main__':
-	T=int(input())
-	for i in range(T):
-		q = int(input())
-		query = []
-		for _ in range(q):
-			query.append(int(input()))
-		
-		ob = Solution()
-		ans = ob.threeDivisors(query,q)
-		for a in ans:
-			print(a)
-
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        asteroids = list(map(int, input().split()))
+        ob = Solution()
+        res = ob.asteroidCollision(N, asteroids)
+        for val in res:
+            print(val, end = ' ')
+        print()
 # } Driver Code Ends
