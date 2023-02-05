6b9702259088cdf52caa31402176dd3c4ca652a9
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2023-02-05 21:21:03 +0530

    Update intersection-point-in-y-shaped-linked-lists.py

--- Intersection Point in Y Shaped Linked Lists - GFG/intersection-point-in-y-shaped-linked-lists.py
+++ Intersection Point in Y Shaped Linked Lists - GFG/intersection-point-in-y-shaped-linked-lists.py
@@ -1,47 +1,120 @@
 #User function Template for python3
-from math import sqrt
+'''
+	Function to return the value at point of intersection
+	in two linked list, connected in y shaped form.
+	
+	Function Arguments: head_a, head_b (heads of both the lists)
+	
+	Return Type: value in NODE present at the point of intersection
+	             or -1 if no common point.
+
+	Contributed By: Nagendra Jha
+
+	{
+		# Node Class
+		class Node:
+		    def __init__(self, data):   # data -> value stored in node
+		        self.data = data
+		        self.next = None
+	}
+'''
 
 class Solution:
-	def threeDivisors(self, query, q):
-	    m = int(sqrt(max(query)))
-	    prime = [True] * (m + 1)
-	    prime[0], prime[1] = False, False
-	    
-	    for i in range(2, len(prime)):
-	        if not prime[i]:
-	            continue
-	        
-	        for j in range(i*i, len(prime), i):
-	            prime[j] = False
-	    
-	    prime = [i for i, e in enumerate(prime) if e]
-	    
-	    ans = [0] * q
-	    for i in range(q):
-	        for k in prime:
-	            if k*k > query[i]:
-	                break
-	            
-	            ans[i] += 1
-	    
-	    return ans
+    def intersetPoint(self,head1,head2):
+        if head1 is None or head2 is None:
+            return -1
+        
+        p = head1
+        while p.next is not None:
+            p = p.next
+        
+        p.next = head2
+        slow = head1
+        fast = head1
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                break
+        
+        if fast is None:
+            return -1
+        
+        fast = head1
+        while fast:
+            slow = slow.next
+            fast = fast.next
+            if slow == fast:
+                break
+        
+        return slow.data
 
 
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
 
+#Contributed by : Nagendra Jha
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+# Node Class
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        temp=None
+    
+    # creates a new node with given value and appends it at the end of the linked list
+    def append(self, new_node):
+        if self.head is None:
+            self.head = new_node
+            self.temp = self.head
+            return
+        else:
+            self.temp.next = new_node
+            self.temp = self.temp.next
+
 if __name__ == '__main__':
-	T=int(input())
-	for i in range(T):
-		q = int(input())
-		query = []
-		for _ in range(q):
-			query.append(int(input()))
-		
-		ob = Solution()
-		ans = ob.threeDivisors(query,q)
-		for a in ans:
-			print(a)
+    t=int(input())
+    for cases in range(t):
+        x,y,z = map(int,input().strip().split())
+        a = LinkedList()  # create a new linked list 'a'.
+        b = LinkedList()  # create a new linked list 'b'.
+        nodes_a = list(map(int, input().strip().split()))
+        nodes_b = list(map(int, input().strip().split()))
+        nodes_common = list(map(int, input().strip().split()))
+
+        for x in nodes_a:
+            node=Node(x)
+            a.append(node)  # add to the end of the list
+
+        for x in nodes_b:
+            node=Node(x)
+            b.append(node)  # add to the end of the list
+
+        for i in range(len(nodes_common)):
+            node=Node(nodes_common[i])
+            a.append(node)  # add to the end of the list a
+            if i== 0:
+                b.append(node)  # add to the end of the list b, only the intersection
+        ob=Solution()
+        print(ob.intersetPoint(a.head,b.head))
 
 # } Driver Code Ends
