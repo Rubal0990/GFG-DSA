bc7bed226b45b80d01bb197d4bf00863ca899d64
Rubal Bansal <rubal00009@gmail.com>
2023-02-10 21:30:44 +0530

    Python - Map

---
+++ Balloon Everywhere - GFG/balloon-everywhere.py
@@ -0,0 +1,46 @@
+class Solution:
+    def create_Dict(self, word, string): 
+        Dict = {}
+        for i in range(len(string)):
+            if string[i] in word:
+                if string[i] not in Dict:
+                    Dict[string[i]] = 1
+                
+                else:
+                    Dict[string[i]] += 1
+        
+        return Dict
+    
+    def maxInstance(self, s : str) -> int:
+        word = "balloon"
+        Dic1 = self.create_Dict(word, word)
+        Dic2 = self.create_Dict(word, s)
+        
+        if len(Dic1) == len(Dic2):
+            first_key = word[0]
+            min = Dic2[first_key] // Dic1[first_key]
+            
+            for i in Dic2.keys():
+                if Dic2[i] // Dic1[i] < min:
+                    min = Dic2[i] // Dic1[i]
+            
+            return min
+        
+        else:
+            return 0
+
+
+#{ 
+ # Driver Code Starts
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        s = (input())
+        
+        obj = Solution()
+        res = obj.maxInstance(s)
+        
+        print(res)
+
+# } Driver Code Ends
