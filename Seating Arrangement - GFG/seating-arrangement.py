9184285044d5ec5838a124143ad45512336708b8
Rubal Bansal <rubal00009@gmail.com>
2023-04-26 23:21:56 +0530

    Python - Arrays | Greedy

---
+++ Seating Arrangement - GFG/seating-arrangement.py
@@ -0,0 +1,72 @@
+import math
+from typing import List
+
+class Solution:
+    def is_possible_to_get_seats(self, n : int, m : int, seats : List[int]) -> bool:
+        if m == 1 and seats[0] == 0:
+            n -= 1
+            seats[0] = 1
+        
+        if m == 2 and seats[0] == 0 and seats[1] == 0:
+            n -= 1
+            seats[0] = 1
+            
+        
+        if n > math.ceil(m / 2):
+            return False
+        
+        if seats[0] == 0 and seats[1] == 0:
+            seats[0] = 1
+            n -= 1
+        
+        for i in range(1, m-1):
+            if seats[i-1] == 0 and seats[i] == 0 and seats[i+1] == 0:
+                seats[i] = 1
+                n -= 1
+        
+        
+        if seats[m-1] == 0 and seats[m-2] == 0:
+            seats[m-1] = 1
+            n -= 1
+        
+        if n <= 0:
+            return True
+        
+        return False
+
+
+#{ 
+ # Driver Code Starts
+
+
+class IntArray:
+    def __init__(self) -> None:
+        pass
+    def Input(self,n):
+        arr=[int(i) for i in input().strip().split()]#array input
+        return arr
+    def Print(self,arr):
+        for i in arr:
+            print(i,end=" ")
+        print()
+
+
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        n = int(input())
+        
+        
+        m = int(input())
+        
+        
+        seats=IntArray().Input(m)
+        
+        obj = Solution()
+        res = obj.is_possible_to_get_seats(n, m, seats)
+        
+        result_val = "Yes" if res else "No"
+        print(result_val)
+
+# } Driver Code Ends
