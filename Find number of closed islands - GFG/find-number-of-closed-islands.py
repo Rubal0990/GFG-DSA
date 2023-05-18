1820a6753fcb12f987fc2b6c4e25daf122e51b2d
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2023-05-18 18:24:47 +0530

    Update find-number-of-closed-islands.py

--- Find number of closed islands - GFG/find-number-of-closed-islands.py
+++ Find number of closed islands - GFG/find-number-of-closed-islands.py
@@ -13,28 +13,28 @@ class Solution:
 	            if newr>=0 and newr<N and newc>=0 and newc<M and matrix[newr][newc]==1:
 	                dfs(newr, newc)
 	        
-		for i in range(M):
-		    if matrix[0][i] == 1:
-		        dfs(0, i)
-		    
-		    if matrix[N-1][i] == 1:
-		        dfs(N - 1, i)
-		
-		for i in range(N):
-		    if matrix[i][0] == 1:
-		        dfs(i, 0)
-		    
-		    if matrix[i][M-1] == 1:
-		        dfs(i, M - 1)
-		
-		ans = 0
-    		for i in range(N):
-    	    	    for j in range(M):
-    	              if matrix[i][j] == 1:
-    	              dfs(i, j)
-    	              ans += 1
-		
-		return ans
+	    for i in range(M):
+	        if matrix[0][i] == 1:
+		    dfs(0, i)
+
+	        if matrix[N-1][i] == 1:
+		    dfs(N - 1, i)
+
+	    for i in range(N):
+	        if matrix[i][0] == 1:
+		    dfs(i, 0)
+
+	        if matrix[i][M-1] == 1:
+		    dfs(i, M - 1)
+
+	    ans = 0
+	    for i in range(N):
+	        for j in range(M):
+	          if matrix[i][j] == 1:
+	          dfs(i, j)
+	          ans += 1
+
+	    return ans
 
 
 #{ 
