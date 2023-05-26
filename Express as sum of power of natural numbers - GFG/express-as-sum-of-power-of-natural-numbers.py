a0cf50a33715a4e839b0e13f8edce01be3d7f2b5
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2023-05-26 09:43:08 +0530

    Update express-as-sum-of-power-of-natural-numbers.py

--- Express as sum of power of natural numbers - GFG/express-as-sum-of-power-of-natural-numbers.py
+++ Express as sum of power of natural numbers - GFG/express-as-sum-of-power-of-natural-numbers.py
@@ -2,21 +2,21 @@
 class Solution:
 	def numOfWays(self, n, x):
 		powers = []
-        for i in range(1, n + 1):
-            v = i ** x
-            if v > n:
-                break
+        	for i in range(1, n + 1):
+            		v = i ** x
+            		if v > n:
+                		break
             
-            powers.append(i ** x)
+            		powers.append(i ** x)
         
-        dp = [0] * (n + 1)
-        dp[0] = 1
+        	dp = [0] * (n + 1)
+        	dp[0] = 1
         
-        for c in powers:
-            for j in range(n, c - 1, -1):
-                dp[j] += dp[j - c]
+        	for c in powers:
+            		for j in range(n, c - 1, -1):
+                		dp[j] += dp[j - c]
         
-        return dp[n] % (10 ** 9 + 7)
+        	return dp[n] % (10 ** 9 + 7)
 
 
 #{ 
