accca1d69f8bb336bd1549ead38f14726fa6d802
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2023-06-12 07:15:39 +0530

    Update rod-cutting.py

--- Rod Cutting - GFG/rod-cutting.py
+++ Rod Cutting - GFG/rod-cutting.py
@@ -1,26 +1,38 @@
 #User function Template for python3
 
 class Solution:
-    def findTime(self, S1, S2):
-        curr = 0
-        sum = 0
+    def cutRod(self, price, n):
+        arr = [0] * (n+1)
+        arr[1] = price[0]
         
-        for i in S2:
-            sum += abs(S1.index(i)-curr)
-            curr = S1.index(i)
+        for i in range(2, n+1):
+            m = 0
+            for j in range(1, i):
+                m = max(m, arr[j] + arr[i-j])
+            
+            m = max(m, price[i-1])
+            arr[i] = m
         
-        return sum
+        return arr[-1]
+
 
 #{ 
-#  Driver Code Starts
+ # Driver Code Starts
 #Initial Template for Python 3
 
-if __name__ == '__main__': 
-    t = int (input ())
-    for _ in range (t):
-        S1=input()
-        S2=input()
-        
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        n = int(input())
+        a = [int(x) for x in input().strip().split()]
         ob = Solution()
-        print(ob.findTime(S1,S2))
+        print(ob.cutRod(a, n))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
 # } Driver Code Ends
