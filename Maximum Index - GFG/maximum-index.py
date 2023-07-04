349fda4dffb919446693df2c76abf80fb30e6b4d
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2023-07-03 19:46:37 -0700

    Update maximum-index.py

--- Maximum Index - GFG/maximum-index.py
+++ Maximum Index - GFG/maximum-index.py
@@ -1,56 +1,32 @@
 #User function Template for python3
 
 class Solution:
-    def maxIndexDiff(self, A, n): 
-        if n == 1:
-            return 0
+	def maxIndexDiff(self, arr, n):
+		for i in range(n):
+            arr[i] = (i, arr[i])
         
-        s = 0
-        L = [A[0]]
-        R = [-1] * n
-        r = A[n-1]
+        arr.sort(key=lambda x:x[1])
+        diff = 0
+        far = float("-inf")
         
-        for i in range(1, n):
-            L.append(min(A[i], L[i-1]))
-        
-        for j in range(n-1, -1, -1):
-            if r < A[j]:
-                r = A[j]
-            
-            R[j] = max(r, A[j])
-        
-        i, j = 0, 0
-        while i < n and j < n:
-            if L[i] <= R[j]:
-                w = j - i
-                s = max(s, w)
-                j += 1
+        for i in range(n-1, -1, -1):
+            curr = arr[i][0]
+            if curr > far:
+                far = curr
             else:
-                i += 1
+                diff = max(diff, far - curr)
         
-        return s
+        return diff
 
 
 #{ 
  # Driver Code Starts
-#Initial Template for Python 3
-
-import math
-
-def main():
-        T=int(input())
-        while(T>0):
-            
-            n=int(input())
-            
-            arr=[int(x) for x in input().strip().split()]
-            ob=Solution()
-            print(ob.maxIndexDiff(arr,n))
-            
-            
-            T-=1
-
-
 if __name__ == "__main__":
-    main()
+	t = int(input())
+	while(t>0):
+		num = int(input())
+		arr = [int(x) for x in input().strip().split()]
+		ob = Solution()
+		print(ob.maxIndexDiff(arr,num))
+		t-=1
 # } Driver Code Ends
