f23ecf70574df26675a207f0f6b8cc85b99abbcd
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2023-07-17 21:23:54 +0530

    Update first-non-repeating-character-in-a-stream.py

--- First non-repeating character in a stream - GFG/first-non-repeating-character-in-a-stream.py
+++ First non-repeating character in a stream - GFG/first-non-repeating-character-in-a-stream.py
@@ -2,29 +2,32 @@
 from collections import deque
 
 class Solution:
-	def FirstNonRepeating(self, A):
-		q = deque()
-		ans = ""
-		mp = {}
-		
-		for i in range(len(A)):
-		    if A[i] in mp:
-		        mp[A[i]] += 1
-		        while q and (q[0]==A[i] or mp[q[0]]>1):
-		            q.popleft()
-	        else:
-	            mp[A[i]] = 1
-	            q.append(A[i])
-	        
-	        if not q:
-	            ans += '#'
-	        else:
-	            ans += q[0]
-		            
+    def FirstNonRepeating(self, A):
+        q = deque()
+        ans = ""
+        mp = {}
+        
+        for i in range(len(A)):
+            if A[i] in mp:
+                mp[A[i]] += 1
+                while q and (q[0]==A[i] or mp[q[0]]>1):
+                    q.popleft()
+            
+            else:
+                mp[A[i]] = 1
+                q.append(A[i])
+            
+            if not q:
+                ans += '#'
+            else:
+                ans += q[0]
+                    
         return ans
+
 
 #{ 
  # Driver Code Starts
+
 #Initial Template for Python 3
 
 if __name__ == '__main__':
