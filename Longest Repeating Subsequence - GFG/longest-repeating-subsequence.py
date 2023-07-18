033492eca9e563d0a9bd2ef84e181f5687ccb482
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2023-07-18 22:04:46 +0530

    Update longest-repeating-subsequence.py

--- Longest Repeating Subsequence - GFG/longest-repeating-subsequence.py
+++ Longest Repeating Subsequence - GFG/longest-repeating-subsequence.py
@@ -1,29 +1,22 @@
 #User function Template for python3
 
 class Solution:
-	def LongestRepeatingSubsequence(self, str):
-		n = len(str)
-		p = str 
-		m = [[0  for i in range(n+1)] for i in range(n+1)]
+    def LongestRepeatingSubsequence(self, str):
+        n = len(str)
+        dp = [[0] * (n+1) for _ in range(n+1)]
 		
-		for i in range(n+1):
-		    for j in range(n+1):
-		        if i == 0 or j == 0:
-		            m[i][j] = 0
-		
-		for i in range(1, n+1):
-		    for j in range(1, n+1):
-		        
-		        if str[i-1] == p[j-1] and i != j:
-		            m[i][j] = m[i-1][j-1] + 1
-		        else:
-		            m[i][j] = max(m[i-1][j], m[i][j-1])
-		
-		return m[n][n]
-		
+        for i in range(1, n + 1):
+            for j in range(1, n + 1):
+                if str[i-1] == str[j-1] and i != j:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+        
+        return dp[n][n]
+
 
 #{ 
-#  Driver Code Starts
+ # Driver Code Starts
 #Initial Template for Python 3
 
 if __name__ == '__main__':
